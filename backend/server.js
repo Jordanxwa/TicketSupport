cd0237254a8451499f5edbe14e199c4efe0c8c65
Jordan Wallace-Addai <jordanwallace1223@gmail.com>
2024-02-05 23:04:08 -0500

    created routes, user controller for cleaner code

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log('Server!');
+const express = require('express');
+const dotenv = require('dotenv').config();
+// Calling port # from env file
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ test: 'JSON data test' });
+});
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
