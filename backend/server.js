aca65008e9a9e515c46af266afb60a44975f31af
Jordan Wallace-Addai <jordanwallace1223@gmail.com>
2024-02-07 02:05:33 -0500

    Added error middleware, asyncHandler,  connected to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 // Calling port # from env file
 const PORT = process.env.PORT || 8000;
 
+// Connect to db
+connectDB();
+
 const app = express();
+
+// Allowing data to show
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.json({ test: 'JSON data test' });
@@ -11,5 +21,7 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
