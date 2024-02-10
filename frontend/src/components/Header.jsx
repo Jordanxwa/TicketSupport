a156f1fe771d09816e0c711e3cc7fd947fbd71ec
Jordan Wallace-Addai <jordanwallace1223@gmail.com>
2024-02-10 18:30:44 -0500

    fixed loading problem with header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,23 +3,23 @@ import {Link} from 'react-router-dom'
 
 function Header() {
   return (
-    <Header className='header'>
+    <header className='header'>
       <div className="logo">
         <Link to='/'>Support Desk</Link>
       </div>
       <ul>
         <li>
           <Link to="/login">
-            <FaSignInAlt />
+            <FaSignInAlt /> Login
           </Link>
         </li>
         <li>
           <Link to="/register">
-            <FaUser />
+            <FaUser /> Register
           </Link>
         </li>
       </ul>
-    </Header>
+    </header>
   )
 }
 
