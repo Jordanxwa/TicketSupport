c3b3e1e2ef1c85183797d4449d1e82f49da07f2f
Jordan Wallace-Addai <jordanwallace1223@gmail.com>
2024-03-26 20:13:13 -0400

    preparing for deployment

--- backend/server.js
+++ backend/server.js
@@ -16,23 +16,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => {
+  res.json({ test: 'JSON data test' });
+});
+
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
-
-// Serve Frontend
-if (process.env.NODE_ENV === 'production') {
-  // Set build folder as static
-  app.use(express.static(path.join__dirname, '../frontend/build'));
-
-  app.get('*', (req, res) =>
-    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to the Support Desk API' });
-  });
-}
 
 app.use(errorHandler);
 
