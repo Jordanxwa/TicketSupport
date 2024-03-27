768041b79f484575680426d3980b2b460b95adb9
Jordan Wallace-Addai <jordanwallace1223@gmail.com>
2024-03-26 20:26:14 -0400

    preparing for deployment

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,10 @@ app.get('/', (req, res) => {
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
 
+app.get('*', (req, res) =>
+  res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+);
+
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
